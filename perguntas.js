fd75c2810096ef774d99f96fa99bdb30b55ac29e
jayjayrockstars <jenniferalmeida45sp@gmail.com>
2024-10-11 12:42:38 -0300

    Ajustando codigo

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o unico continente com terra nos quatro hemisférios?',
+    'A África.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é HTML',
+    'HTML é a linguagem de Marcação de hipertexto.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz gato em Inglês?',
+    'gato em ingles é cat'
+)
